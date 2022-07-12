e5f97baee031ebfd19f871149bb9f4576b8b1f5f
@omi-coder <omi-ufa@yandex.ru>
2022-07-12 09:02:24 +0300

    Поля таблицы заполнены

--- models.py
+++ models.py
@@ -1,17 +1,8 @@
+import json
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from sqlalchemy import or_, desc, func
-import data
+from config import db
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSON_AS_ASCII'] = False
-# app.url_map.strict_slashes = False
-db = SQLAlchemy(app)
 
 # Создаем модели
 
@@ -25,71 +16,127 @@ class User(db.Model):
     role = db.Column(db.String(100))
     phone = db.Column(db.String(100))
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role,
+            "phone": self.phone
+        }
+
+
 class Order(db.Model):
     __tablename__ = 'order'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(100))
-    start_date = db.Column(db.String(100))
-    end_date = db.Column(db.String(100))
+    start_date = db.Column(db.Integer)
+    end_date = db.Column(db.Integer)
     address = db.Column(db.String(100))
-    price = db.Column(db.Float)
-    customer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    executor_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    customer = relationship("User", foreign_keys=[customer_id])
-    executor = relationship("User", foreign_keys=[executor_id])
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # customer = relationship("User", foreign_keys=[customer_id])
+    # executor = relationship("User", foreign_keys=[executor_id])
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "address": self.address,
+            "price": self.price,
+            "customer_id": self.customer_id,
+            "executor_id": self.executor_id
+        }
+
 
 class Offer(db.Model):
     __tablename__ = 'offer'
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey("order.id"))
-    executor_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    order = relationship("Order", foreign_keys=[order_id])
-    executor = relationship("User", foreign_keys=[executor_id])
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # order = relationship("Order", foreign_keys=[order_id])
+    # executor = relationship("User", foreign_keys=[executor_id])
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "executor_id": self.executor_id
+        }
+
+# Заполняем таблицы данными
+
+def insert_data_user(input_data):
+    for row in input_data:
+        db.session.add(
+            User(
+            id=row.get("id"),
+            first_name=row.get("first_name"),
+            last_name=row.get("last_name"),
+            age=row.get("age"),
+            email=row.get("email"),
+            role=row.get("role"),
+            phone=row.get("phone")
+            )
+        )
+    db.session.commit()
+
+
+def insert_data_order(input_data):
+    for row in input_data:
+        db.session.add(
+            Order(
+                id=row.get("id"),
+                name=row.get("name"),
+                description=row.get("description"),
+                start_date=row.get("start_date"),
+                end_date=row.get("end_date"),
+                address=row.get("address"),
+                price=row.get("price"),
+                customer_id=row.get("customer_id"),
+                executor_id=row.get("executor_id")
+            )
+        )
+
+    db.session.commit()
+
+def insert_data_offer(input_data):
+    for row in input_data:
+        db.session.add(
+            Offer(
+                id=row.get("id"),
+                order_id=row.get("order_id"),
+                executor_id=row.get("executor_id")
+                )
+            )
+
+    db.session.commit()
+
+def get_all_users():
+    result = []
+    users = User.query.all()
+    for row in users:
+        result.append(row.to_dict())
+    return result
 
 db.create_all()
 
-# Добавляем данные в таблицы
-for user in data.USERS:
-    db.session.add(User(
-       id=user['id'],
-       first_name=user['first_name'],
-       last_name=user['last_name'],
-       age=user['age'],
-       email=user['email'],
-       role=user['role'],
-       phone=user['phone'])
-    )
-    db.session.commit()
+def init_db():
+    with open("data/user.json") as file:
+        insert_data_user(json.load(file))
 
-for order in data.ORDERS:
-    db.session.add(Order(
-        id=order['id'],
-        name=order['name'],
-        description=order['description'],
-        start_date=order['start_date'],
-        end_date=order['end_date'],
-        address=order['address'],
-        price=order['price'],
-        customer_id=order['customer_id'],
-        executor_id=order['executor_id'])
-    )
-    db.session.commit()
+    with open("data/order.json") as file:
+        insert_data_order(json.load(file))
 
-for offer in data.OFFERS:
-    db.session.add(Offer(
-        id=offer['id'],
-        order_id=offer['order_id'],
-        executor_id=offer['executor_id'])
-    )
-    db.session.commit()
-
-db.session.close()
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
-#
-#
-# if __name__ == '__main__':
-#     app.run(Debug=True)
+    with open("data/offer.json") as file:
+        insert_data_offer(json.load(file))
 
